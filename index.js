eefbc35325c3e9551012037bb44b7334d97c5ad7
yukiti224gt <mamekiti084@gmail.com>
2021-07-14 11:13:52 +0900

    ADD: loading画面追加

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+// ハンバーガーメニュー
 const ham = document.getElementById('ham');
 const menu_wrapper = document.getElementById('menu_wrapper');
 ham.addEventListener('click', function () {
   ham.classList.toggle(('clicked'))
   menu_wrapper.classList.toggle(('clicked'))
 })
+
+// ローディング画面
+window.onload = function () {
+  const spinner = document.getElementById('loading');
+  spinner.classList.toggle('loaded')
+}
