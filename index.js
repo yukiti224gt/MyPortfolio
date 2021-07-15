729d86f36fd816573815a5b53f0856f10156eca3
yukiti224gt <mamekiti084@gmail.com>
2021-07-15 14:15:08 +0900

    ADD: リファクタリング

--- index.js
+++ index.js
@@ -13,19 +13,19 @@ window.onload = function () {
 }
 
 //　スムーズスクロール
-const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
-  for (let i = 0; i < smoothScrollTrigger.length; i++){
-    smoothScrollTrigger[i].addEventListener('click', (e) => {
-      e.preventDefault();
-      let href = smoothScrollTrigger[i].getAttribute('href');
-      let targetElement = document.getElementById(href.replace('#', ''));
-      const rect = targetElement.getBoundingClientRect().top;
-      const offset = window.pageYOffset;
-      const gap = 300;
-      const target = rect + offset - gap;
-      window.scrollTo({
-        top: target,
-        behavior: 'smooth',
-      });
-    });
-  }
+// const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
+//   for (let i = 0; i < smoothScrollTrigger.length; i++){
+//     smoothScrollTrigger[i].addEventListener('click', (e) => {
+//       e.preventDefault();
+//       let href = smoothScrollTrigger[i].getAttribute('href');
+//       let targetElement = document.getElementById(href.replace('#', ''));
+//       const rect = targetElement.getBoundingClientRect().top;
+//       const offset = window.pageYOffset;
+//       const gap = 300;
+//       const target = rect + offset - gap;
+//       window.scrollTo({
+//         top: target,
+//         behavior: 'smooth',
+//       });
+//     });
+//   }
