7028e4595280b165d932093e073aced946493110
yukiti224gt <mamekiti084@gmail.com>
2021-07-19 16:05:26 +0900

    ADD: 矢印動き

--- index.js
+++ index.js
@@ -12,20 +12,25 @@ window.onload = function () {
   spinner.classList.toggle('loaded')
 }
 
-//　スムーズスクロール
-// const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
-//   for (let i = 0; i < smoothScrollTrigger.length; i++){
-//     smoothScrollTrigger[i].addEventListener('click', (e) => {
-//       e.preventDefault();
-//       let href = smoothScrollTrigger[i].getAttribute('href');
-//       let targetElement = document.getElementById(href.replace('#', ''));
-//       const rect = targetElement.getBoundingClientRect().top;
-//       const offset = window.pageYOffset;
-//       const gap = 300;
-//       const target = rect + offset - gap;
-//       window.scrollTo({
-//         top: target,
-//         behavior: 'smooth',
-//       });
-//     });
-//   }
+// ふわっと出る処理
+function showElementAnimation() {
+  let element = document.getElementsByClassName('animation');
+  if (!element) return;
+  
+  let showTiming = window.innerHeight > 768 ? 200 : 40;
+  let scrollY = window.pageYOffset;
+  let windowH = window.innerHeight;
+
+  for (let i = 0; i < element.length; i++) {
+    let elemClientRect = element[i].getBoundingClientRect();
+    let elemY = scrollY + elemClientRect.top;
+    if (scrollY + windowH - showTiming > elemY) {
+      element[i].classList.add('is-show');
+    }
+    else if (scrollY + windowH < elemY) {
+      element[i].classList.remove('is-show');
+    }
+  }
+}
+showElementAnimation();
+window.addEventListener('scroll', showElementAnimation);
